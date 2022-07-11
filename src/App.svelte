aabc8df3d7e94740395b40e161cbedf90940933b
Stefan Reifenberg <stefan.reifenberg@googlemail.com>
2022-07-11 14:34:01 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,44 @@
+<script>
+  import { SphereBufferGeometry, MeshBasicMaterial, DoubleSide } from 'three';
+  import { Canvas, PerspectiveCamera, AmbientLight, Mesh, OrbitControls } from 'threlte';
+  import Hexglobe from "./lib/Hexglobe.svelte";
+
+  let hexData;
+  
+  fetch('./countries.geo.json')
+    .then((response) => response.json())
+    .then((data) => {
+      hexData = data.features;
+    })
+    .catch((error) => {
+      console.log(error);
+      return [];
+    });
+    
+  </script>
+  
+  <div class="canvas-wrapper">
+    <Canvas>
+      <PerspectiveCamera fov={55} position={{ z: -25 }}>
+        <OrbitControls autoRotate autoRotateSpeed={0.8} />
+      </PerspectiveCamera>
+      <AmbientLight intensity={0.9}  />
+      
+      <Mesh
+        geometry={new SphereBufferGeometry()}
+        material={new MeshBasicMaterial({ color: '#1F2022', side: DoubleSide })}
+        scale={400}
+      />
+  
+      {#if hexData}
+        <Hexglobe hexdata={hexData} />
+      {/if}
+    </Canvas>
+  </div>
+  
+  <style>
+    .canvas-wrapper {
+      width: 100%;
+      height: 100vh;
+    }
+  </style>
---
+++ src/lib/Hexglobe.svelte
@@ -0,0 +1,24 @@
+
+<script>
+	import { Object3DInstance, useFrame } from 'threlte';
+	import ThreeGlobe from 'three-globe';
+
+    export let hexdata;
+    
+    let globe = new ThreeGlobe()
+      .globeImageUrl('//unpkg.com/three-globe/example/img/earth-dark.jpg')
+      .hexPolygonsData(hexdata)
+      .hexPolygonResolution(3)
+      .hexPolygonAltitude(0.001)
+      .hexPolygonMargin(0.2)
+      .hexPolygonColor(() => '#b5290d');
+
+        
+	useFrame(() => {
+		globe = globe;
+	});
+	
+</script>
+
+<Object3DInstance object={globe} scale={0.07} />
+
