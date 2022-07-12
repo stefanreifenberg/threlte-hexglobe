33cd94e225cdaf195860ce95f7328045b486160d
Stefan Reifenberg <stefan.reifenberg@googlemail.com>
2022-07-12 07:44:42 +0200

    add colorScale

--- src/App.svelte
+++ src/App.svelte
@@ -31,7 +31,7 @@
       />
   
       {#if hexData}
-        <Hexglobe hexdata={hexData} />
+        <Hexglobe hexdata={hexData}/>
       {/if}
     </Canvas>
   </div>
--- src/lib/Hexglobe.svelte
+++ src/lib/Hexglobe.svelte
@@ -2,18 +2,22 @@
 <script>
 	import { Object3DInstance, useFrame } from 'threlte';
 	import ThreeGlobe from 'three-globe';
+    import * as d3 from 'd3';
 
     export let hexdata;
-    
+    const colorAccessor = d => d.properties.GDP_MD_EST;
+    const colorScale = d3.scaleSequential()
+            .domain(d3.extent(hexdata, colorAccessor))
+            .interpolator(d3.interpolateViridis);
+   
     let globe = new ThreeGlobe()
       .globeImageUrl('./earth-dark.jpeg')
       .hexPolygonsData(hexdata)
       .hexPolygonResolution(3)
       .hexPolygonAltitude(0.001)
       .hexPolygonMargin(0.2)
-      .hexPolygonColor(() => '#b5290d');
+      .hexPolygonColor( d => colorScale(colorAccessor(d)))
 
-        
 	useFrame(() => {
 		globe = globe;
 	});
