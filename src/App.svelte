7b96c36f094a7393ff41ff593ea84c16250a650f
Stefan Reifenberg <stefan.reifenberg@googlemail.com>
2022-07-13 21:09:25 +0200

    scale altitude

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script>
   import { SphereBufferGeometry, MeshBasicMaterial, DoubleSide } from 'three';
-  import { Canvas, PerspectiveCamera, AmbientLight, Mesh, OrbitControls } from 'threlte';
+  import { Canvas, PerspectiveCamera, AmbientLight, Mesh, OrbitControls, Text } from 'threlte';
   import Hexglobe from "./lib/Hexglobe.svelte";
 
   let hexData;
@@ -19,17 +19,17 @@
   
   <div class="canvas-wrapper">
     <Canvas>
-      <PerspectiveCamera fov={55} position={{ z: -25 }}>
-        <OrbitControls autoRotate autoRotateSpeed={0.8} />
+      <PerspectiveCamera fov={90} position={{ x: 0, y:0, z: -12 }}>
+        <OrbitControls  autoRotate autoRotateSpeed={0.8} />
       </PerspectiveCamera>
       <AmbientLight intensity={0.9}  />
       
       <Mesh
         geometry={new SphereBufferGeometry()}
-        material={new MeshBasicMaterial({ color: '#1F2022', side: DoubleSide })}
+        material={new MeshBasicMaterial({ color: 'black', side: DoubleSide })}
         scale={400}
       />
-  
+
       {#if hexData}
         <Hexglobe hexdata={hexData}/>
       {/if}
--- src/lib/Hexglobe.svelte
+++ src/lib/Hexglobe.svelte
@@ -1,22 +1,30 @@
 
 <script>
-	import { Object3DInstance, useFrame } from 'threlte';
+	import { Object3DInstance, useFrame, useThrelte } from 'threlte';
 	import ThreeGlobe from 'three-globe';
     import * as d3 from 'd3';
+    import  { displayData } from './data';
 
     export let hexdata;
+
     const colorAccessor = d => d.properties.GDP_MD_EST;
     const colorScale = d3.scaleSequential()
             .domain(d3.extent(hexdata, colorAccessor))
             .interpolator(d3.interpolateViridis);
+
+    const yScale = d3.scaleLinear()
+        .domain(d3.extent(hexdata, colorAccessor))
+		.range([0, 8])
+		.nice(true)        
    
     let globe = new ThreeGlobe()
       .globeImageUrl('./earth-dark.jpeg')
       .hexPolygonsData(hexdata)
       .hexPolygonResolution(4)
-      .hexPolygonAltitude(0.001)
+      .hexPolygonAltitude(d => yScale(colorAccessor(d)))
       .hexPolygonMargin(0.2)
       .hexPolygonColor( d => colorScale(colorAccessor(d)))
+      
 
 	useFrame(() => {
 		globe = globe;
@@ -24,5 +32,5 @@
 	
 </script>
 
-<Object3DInstance object={globe} scale={0.07} />
+<Object3DInstance object={globe} scale={0.02} castShadow receiveShadow/>
 
